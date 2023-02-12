88ab805b80753ca8d5c7024a1266977debd3ba84
Folawewo Anthony <famwallexie@gmail.com>
2023-02-12 18:14:42 +0100

    Update A Todo

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Get all to-do items
 app.get('/todos', async (req, res) => {
   const todos = await Todo.find({});
   res.json(todos);
 });
 
+// Add a new to-do item
 app.post('/todos', async (req, res) => {
   const todo = new Todo({
     task: req.body.task,
@@ -20,5 +22,22 @@ app.post('/todos', async (req, res) => {
   await todo.save();
   res.json(todo);
 });
+
+// Update a to-do item
+app.put('/todos/:id', async (req, res) => {
+  const todo = await Todo.findByIdAndUpdate(
+    req.params.task,
+    {
+      task: req.body.task,
+      complete: req.body.completed,
+    },
+    {
+      new: true,
+    }
+  );
+  res.json(todo);
+});
+
+
 
 module.exports = app;
