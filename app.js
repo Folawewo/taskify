b221e5038f9d3bd85b43b6338159545a7177f36e
Folawewo Anthony <famwallexie@gmail.com>
2023-02-12 17:35:23 +0100

    Get All Todos

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Todo = require('./models/todo.mongo');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/todos', async (req, res) => {
+  const todos = await Todo.find({});
+  res.json(todos);
+});
 
 module.exports = app;
